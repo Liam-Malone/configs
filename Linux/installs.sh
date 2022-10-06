36d306020052fa670b65891a48e6b6b1e15d63dd
Liam <liammaloneirlusa@gmail.com>
2022-10-06 14:39:43 +0100

    fixed up install script and pkglist

--- Linux/installs.sh
+++ Linux/installs.sh
@@ -1,39 +1,46 @@
 #!/bin/sh
 clear;echo ""; 
-echo "If you haven't already, cd to the directory this script is stored in before continuing."; sleep 3; echo "";
+echo -e "\e[1;35mIf you haven't already, cd to the directory this script is stored in before continuing."; sleep 3; echo "";
 fromdir=$(pwd)
-echo "running from $fromdir"
+echo -e "\e[1;34mrunning from $fromdir"
 cd $HOME
-mkdir pictures; mkdir pictures/screenshots; mkdir documents; mkdir downloads
-echo "[%] Upgrading system..."; sleep 1
+mkdir pictures; mkdir pictures/screenshots; mkdir documents; mkdir downloads; mkdir videos
+echo -e "\e[1;34m[%] Upgrading system..."; sleep 1
 sudo pacman -Syu
 
 cd $fromdir
-echo "[%] installing packages from given list..."; sleep 1
-sudo pacman -S --needed - < pkg_installs.txt; 
+echo -e "\e[1;32m[%] installing packages from given list..."; sleep 1
+sudo pacman -S --needed - < pkglist.txt; 
+
+echo -e "\e[1;32m[%] installing flatpaks..."; sleep 1
+flatpak install com.obsproject.Studio org.kde.kdenlive com.valvesoftware.Steam com.github.tchx84.Flatseal
+
+echo -e "\e[1;32m[%] installing aur packages..."
+sleep 1
+paru -S brave-bin pfetch spotify-tui nitch cava polymc-bin
 
 cd $HOME
-echo "[%] grabbing dwm..."
+echo -e "\e[1;32m[%] grabbing dwm..."
 git clone https://github.com/Liam-Malone/dwm
-echo "[%] building dwm..."; cd $HOME/dwm; sudo make clean install; cd $HOME
+echo -e "\e[1;34m[%] building dwm..."; cd $HOME/dwm; sudo make clean install; cd $HOME
 echo "$(cat dwm/config.h | XK_)" > $HOME/documents/keybinds
 
-echo "[%] grabbing dmenu..."
+echo -e "\e[1;32m[%] grabbing dmenu..."
 git clone https://github.com/Liam-Malone/dmenu
-echo "[%] building dmenu..."; cd $HOME/dmenu; sudo make clean install; cd $HOME
+echo -e "\e[1;34m[%] building dmenu..."; cd $HOME/dmenu; sudo make clean install; cd $HOME
 
-echo "[%] grabbing slstatus..."
+echo -e "\e[1;32m[%] grabbing slstatus..."
 git clone https://github.com/Liam-Malone/slstatus
-echo "[%] building slstatus..."; cd $HOME/slstatus; sudo make clean install; cd $HOME
+echo -e "\e[1;34m[%] building slstatus..."; cd $HOME/slstatus; sudo make clean install; cd $HOME
 
-echo "[%] grabbing paru (aur helper)..."
+echo -e "\e[1;32m[%] grabbing paru (aur helper)..."
 cd $HOME; git clone https://aur.archlinux.org/paru
-echo "[%] building paru..."; cd $HOME/paru; makepkg
+echo -e "\e[1;34m[%] building paru..."; cd $HOME/paru; makepkg
 sudo cp pkg/paru/usr/bin/paru /usr/bin/
 
 cd $fromdir
 
-echo "[%] moving onto configs..."; sleep 1
+echo -e "\e[1;34m[%] moving onto configs..."; sleep 1
 cp bashrc $HOME/.bashrc
 cp -R scripts $HOME/
 
@@ -44,17 +51,12 @@ cp xinitrc $HOME/.xinitrc
 
 cd $HOME
 
-echo "[%] installing flatpaks..."; sleep 1
-flatpak install com.obsproject.Studio org.kde.kdenlive com.valvesoftware.Steam
 
-echo "[%] installing aur packages..."
-sleep 1
-paru -S brave-bin pfetch spotify-tui nitch cava polymc-bin
 echo ""; echo ""
-echo ""; echo "[%] grabbing wallpapers..."; cd $HOME/pictures
+echo ""; echo -e "\e[1;32m[%] grabbing wallpapers..."; cd $HOME/pictures
 git clone https://gitlab.com/dwt1/wallpapers; cd $HOME
-echo "[%] Assuming no failures; install is complete :)"
+echo -e "\e[1;32m[%] Assuming no failures; install is complete :)"
 sleep 1
-echo "Enjoy your new system :)"
+echo -e "Enjoy your new system :)"
 sleep 1
 startx
